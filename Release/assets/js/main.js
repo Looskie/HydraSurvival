26283072cc798d3658e6418207b32f308ec962db
looskie <hello@looskie.com>
2020-06-12 17:08:29 -0400

    Big update (Close to release)

--- Release/assets/js/main.js
+++ Release/assets/js/main.js
@@ -15,26 +15,32 @@ $(document).ready(function(){
     );
 });
 
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $("nav");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
+
 function copyIp(element) {
-    var range, selection, worked;
-  
-    if (document.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText(element);
-      range.select();
-    } else if (window.getSelection) {
-      selection = window.getSelection();        
-      range = document.createRange();
-      range.selectNodeContents(element);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    }
-    
-    try {
-      document.execCommand('copy');
-      window.getSelection().removeAllRanges();
-    }
-    catch (err) {
-      alert('unable to copy text');
-    }
+  var range, selection, worked;
+
+  if (document.body.createTextRange) {
+    range = document.body.createTextRange();
+    range.moveToElementText(element);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection(); range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
   }
+    
+  try {
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  }
+  catch (err) {
+    alert('unable to copy text');
+  }
+}
