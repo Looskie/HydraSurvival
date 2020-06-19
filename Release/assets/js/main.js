1d9cdb14e58448c018fedf5c76f48893c0d619a2
looskie <50378828+Looskie@users.noreply.github.com>
2020-06-19 03:33:18 -0400

    Fixes
    Added copied ip alert
    Optimized Main.js
    fixed bugs

--- Release/assets/js/main.js
+++ Release/assets/js/main.js
@@ -1,60 +1,64 @@
-$(window).on('load', function() {$('#loader').delay(1500).fadeOut('slow')});
-$(document).ready(function(){
-  function toggleSidebar() {
-    $(".button").toggleClass("active");
-    $("main").toggleClass("move-to-left");
-    if ($(".sidebar-item").hasClass("active")) {
-      setTimeout(function() {$(".sidebar-item").toggleClass("active")},600);
-    } else {
-      $(".sidebar-item").toggleClass("active");
-    }
-  }
-  // ! Put auto callpaser here //
-  
-  $(".button").on("click tap", function() {
-    toggleSidebar();
-  });
-    $('.dark').hover(
-        // trigger when mouse hover
-        function(){
-            $(this).animate({
-                marginTop: "-=1%",
-            },200);
-        },
-        function(){
-            $(this).animate({
-                marginTop: "0%"
-            },200);
-        }
-    );
-});
+// PRELOADER
+$(window).on('load', function(){$('#loader').delay(1500).fadeOut('slow')});
 
-$(function () {
-  $(document).scroll(function () {
-	  var $nav = $("nav");
-	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-	});
-});
-
-function copyIp(element) {
-  var range, selection, worked;
-
-  if (document.body.createTextRange) {
-    range = document.body.createTextRange();
-    range.moveToElementText(element);
-    range.select();
-  } else if (window.getSelection) {
-    selection = window.getSelection(); range = document.createRange();
-    range.selectNodeContents(element);
+// Copy IP
+function copyIp(e){
+  if(window.getSelection){
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(e);
     selection.removeAllRanges();
     selection.addRange(range);
-  }
-    
-  try {
+  }try{
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
-  }
-  catch (err) {
-    alert('unable to copy text');
+    Swal.fire({
+      icon: 'success',
+      title: 'The IP has been copied!',
+      showConfirmButton: false,
+      timer: 1000
+    })
+  }catch(err) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Yikes!',
+      text: 'Something went wrong!',
+      footer: 'Contact us at support@hydrasurvival.us'
+    })
   }
 }
+
+// Everything that needs to load during the ready statement
+$(document).ready(function(){
+    // Toggle Nav
+    function toggleNav(){
+        $(".button, .main-nav").toggleClass("active");
+        $("main").toggleClass("move-to-left");
+        if($(".sidebar-item").hasClass("active")) {
+          setTimeout(function() {$(".sidebar-item").toggleClass("active")},600);
+        }else {
+          $(".sidebar-item").toggleClass("active");
+        }
+    }
+    // Button listner
+    $('.button').on('click tap', function(){
+        toggleNav();
+    })
+    
+    // News Animation 
+    $('.dark').hover(function(){
+        $(this).animate({
+            marginTop: "-=1%",
+        },200);
+    },function(){
+        $(this).animate({
+            marginTop: "0%"
+        },200);
+    });
+
+    // Nav Scroll
+    $(document).scroll(function(){
+        var $nav = $('nav');
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    })
+  });
